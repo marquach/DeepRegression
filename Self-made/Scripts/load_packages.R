6dac8e433e037e983e6b24958afe3d8335012e2c
marquach <ch.marquardt@campus.lmu.de>
2023-01-17 16:32:38 +0100

    undo change

--- Self-made/Scripts/load_packages.R
+++
@@ -1,6 +0,0 @@
-# packages
-library(ggplot2)
-library(mgcv)
-library(luz)
-library(torch)
-library(deepregression)
--- inst/python/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .custom_train_step import *
-from .model_trainable_para import *
