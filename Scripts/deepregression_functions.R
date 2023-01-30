0e3f5bd7fb697d86084bfe03af5dcc064e767f8b
marquach <ch.marquardt@campus.lmu.de>
2023-01-30 11:17:31 +0100

    Penalty Matrix bei Spline Layer added

--- Scripts/deepregression_functions.R
+++ Scripts/deepregression_functions.R
@@ -15,8 +15,11 @@
 # kernel_regularizer = squaredPenalty(P, 1) gibt es nicht bei Torch
 # Regularisierung direkt bei Loss Berechnung??
 
+# Per hook implementiert, da layer unterschiedlich penalisiert werden können
+
 layer_spline_torch <- function(units = 1L, name, #trainable = TRUE,
-                               kernel_initializer = "glorot_uniform"){
+                               kernel_initializer = "glorot_uniform",
+                         P){
   
   spline_layer <- nn_linear(units, out_features = 1, bias = F)
   
@@ -26,27 +29,56 @@ layer_spline_torch <- function(units = 1L, name, #trainable = TRUE,
       gain = nn_init_calculate_gain(nonlinearity = "linear"))
   }
   
+  # nicht sicher ob P immer symmetrisch ist, deswegen P+P$t() statt 2*P
+  # glaub aber schon
+  spline_layer$parameters$weight$register_hook(function(grad){
+    grad + torch_matmul((P+P$t()), spline_layer$weight$t())$t()
+  })
+  
+  
   spline_layer
 }
 
-load(file = "test_data/parsed_formulas_contents.RData")
-# makeInputs <- function (pp, param_nr) {
-#  lapply(pp, function(ap) {
-#    if (length(ap$input_dim) > 1)   Wenn Tensor
-#      inp <- as.list(as.integer(ap$input_dim))
 
-#    else inp <- list(as.integer(ap$input_dim))
+torch_model <-  function(submodules_list){
+  nn_module(
+    classname = "torch_model",
+    initialize = function() {
+      self$subnetworks <- nn_module_list(submodules_list)
+    },
+    
+    forward = function(dataset_list) {
+      subnetworks <- lapply(1:length(self$subnetworks), function(x){
+        self$subnetworks[[x]](dataset_list[[x]])
+      })
+      Reduce(f = "+", x = subnetworks)}
+  )}
 
-#    return(tf$keras$Input(shape = inp, 
-#                          name = paste0("input_", 
-#                                        strtrim(make_valid_layername(ap$term),
-#                                                options()$cutoff_names), 
-#                                                     "_", param_nr)))
-#  })
-#}
+# Dataloader müsste dann m.M.n. in deepregression erstellt werden,
+# da außerhalb nicht schön wäre bzw. zu sehr von keras abweicht.
+# Input wird ja eh schon in Form von data gegeben, also sollte das passen
 
-# parsed_formulas_contents ist input
-
-str(parsed_formulas_contents)
-parsed_formulas_contents[[1]][[1]]$input_dim
+get_luz_dataset <- dataset(
+  "deepregression_luz_dataset",
+  
+  initialize = function(df_list, target) {
+    self$df_list <- df_list
+    self$target <- target
+  },
+  
+  .getitem = function(index) {
+    indexes <- lapply(self$df_list, function(x) x[index,])
+    target <- self$target[index]$view(1)
+    # note that the dataloaders will automatically stack tensors
+    # creating a new dimension
+    list(indexes, target)
+  },
+  
+  # Braucht man für dataloader. Wenn Shuffle aus wird es nicht genutzt.
+  # Scheint als ob shuffle so nocht nicht funktioniert. (Später anschauen)
+  .length = function() {
+    length(self$target)
+  }
+  
+)
 
