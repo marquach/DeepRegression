b090ef218e20a4f5176ea3b6b1eff9de921b5c48
marquach <ch.marquardt@campus.lmu.de>
2023-02-09 17:08:31 +0100

    layer_spline_torch added für ifelse Abfrage

--- deepregression-main/R/psplinelayer.R
+++ deepregression-main/R/psplinelayer.R
@@ -81,6 +81,59 @@ layer_spline = function(units = 1L, P, name, trainable = TRUE,
                               name = name, kernel_initializer = kernel_initializer))
 }
 
+#' Function to define spline as Torch layer
+#' 
+#' @param units integer; number of output units
+#' @param P matrix; penalty matrix
+#' @param name string; string defining the layer's name
+#' @param trainable logical; whether layer is trainable
+#' @param kernel_initializer initializer; for basis coefficients
+#' @return Torch layer
+#' @export
+layer_spline_torch <- function(P, units, name, trainable = TRUE,
+                               kernel_initializer = "glorot_uniform"){
+  
+  spline_layer <- nn_linear(units, out_features = 1, bias = FALSE)
+  
+  if (kernel_initializer == "glorot_uniform") {
+    nn_init_xavier_uniform_(
+      tensor = spline_layer$weight,
+      gain = nn_init_calculate_gain(nonlinearity = "linear"))
+  }
+  
+  spline_layer$parameters$weight$register_hook(function(grad){
+    grad + torch_matmul((P+P$t()), spline_layer$weight$t())$t()
+  })
+  
+  if(!trainable) spline_layer$parameters$weight$requires_grad = FALSE
+  
+  spline_layer
+}
+
+#' Function to define a torch layer similiar to a tf dense layer
+#' 
+#' @param units integer; number of output units
+#' @param name string; string defining the layer's name
+#' @param trainable logical; whether layer is trainable
+#' @param kernel_initializer initializer; for basis coefficients
+#' @return Torch layer
+#' @export
+torch_layer_dense <- function(units, name, trainable = TRUE,
+                              kernel_initializer = "glorot_uniform",
+                              use_bias = FALSE){
+  
+  torch_layer <- nn_linear(units, out_features = 1, bias = use_bias)
+  
+  if (kernel_initializer == "glorot_uniform") {
+    nn_init_xavier_uniform_(
+      tensor = torch_layer$weight,
+      gain = nn_init_calculate_gain(nonlinearity = "linear"))
+  }
+  
+  if(!trainable) torch_layer$parameters$weight$requires_grad = FALSE
+  
+  torch_layer
+}
 
 tf_incross = function(w, P) {
   python_path <- system.file("python", package = "deepregression")
