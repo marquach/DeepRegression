5f8ab6b95511524560fb381340422acd83e5b9ab
marquach <ch.marquardt@campus.lmu.de>
2023-02-09 20:32:00 +0100

    torch.dense.layer similar to tf.dense.layer (4 layer_generator)

--- Scripts/deepregression_functions.R
+++ Scripts/deepregression_functions.R
@@ -1,3 +1,29 @@
+#' Function to define a torch layer similiar to a tf dense layer
+#' 
+#' @param units integer; number of output units
+#' @param P matrix; penalty matrix
+#' @param name string; string defining the layer's name
+#' @param trainable logical; whether layer is trainable
+#' @param kernel_initializer initializer; for basis coefficients
+#' @return Torch layer
+#' @export
+torch_layer_dense <- function(units, name, trainable = TRUE,
+                         kernel_initializer = "glorot_uniform"){
+  
+  torch_layer <- nn_linear(units, out_features = 1, bias = FALSE)
+  
+  if (kernel_initializer == "glorot_uniform") {
+    nn_init_xavier_uniform_(
+      tensor = torch_layer$weight,
+      gain = nn_init_calculate_gain(nonlinearity = "linear"))
+  }
+  
+  if(!trainable) torch_layer$parameters$weight$requires_grad = FALSE
+  
+  torch_layer
+}
+
+
 
 #' Function to define spline as Torch layer
 #' 
@@ -8,7 +34,7 @@
 #' @param kernel_initializer initializer; for basis coefficients
 #' @return Torch layer
 #' @export
-layer_spline <- function(P, units, name, trainable = TRUE,
+layer_spline_torch <- function(P, units, name, trainable = TRUE,
                                kernel_initializer = "glorot_uniform"){
   
   spline_layer <- nn_linear(units, out_features = 1, bias = FALSE)
