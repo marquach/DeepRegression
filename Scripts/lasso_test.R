710421b3d1c8fa43a91372e94fd7a68780e03e0e
Christopher Marquardt <ch.marquardt@campus.lmu.de>
2023-03-29 15:53:37 +0200

    inital commit

--- Scripts/lasso_test.R
+++ Scripts/lasso_test.R
@@ -1,5 +1,6 @@
 # simplyconnected_layer_torch Verstehen
 library(torch)
+library(luz)
 source("scripts/deepregression_functions.R")
 devtools::load_all("deepregression-main/")
 # Tib linear lasso
@@ -68,12 +69,21 @@ set.seed(42)
 n <- 1000
 p <- 5
 x <- data.frame(runif(n*p) %>% matrix(ncol=p))
-y <- 2*x[,1] + rnorm(n)
+y <- 2*x[,1] + 3*x[,2] + rnorm(n)
+colnames(x) <- paste("x", 1:5, sep = "")
+
+lm(y ~., data = x)
 
 debugonce(deepregression)
-colnames(x) <- paste("x", 1:5, sep = "")
+
 lasso_test_torch <- deepregression(y = matrix(y), list_of_formulas = list(
   loc = ~ -1 + lasso(x1) + lasso(x2) + lasso(x3) + lasso(x4) + lasso(x5),
+  scale = ~ 1), data = x, engine = "torch",
+  subnetwork_builder = subnetwork_init_torch, model_builder = torch_dr,
+  orthog_options = orthog_control(orthogonalize = F)
+)
+sin_lasso_test_torch <- deepregression(y = matrix(y), list_of_formulas = list(
+  loc = ~ -1 + x1 + x2 + x3 + x4 + x5,
   scale = ~ 1), data = x, engine = "torch",
   subnetwork_builder = subnetwork_init_torch, model_builder = torch_dr,
   orthog_options = orthog_control(orthogonalize = F)
@@ -84,7 +94,13 @@ lasso_test_tf <- deepregression(y = matrix(y), list_of_formulas = list(
   orthog_options = orthog_control(orthogonalize = F)
 )
 debugonce(fit)
-lasso_test_torch %>% fit(epochs = 120, early_stopping = F, validation_split = 0)
+lasso_test_torch %>% fit(epochs = 100, early_stopping = T, validation_split = 0.1)
+lasso_test_torch %>% coef()
+
+sin_lasso_test_torch %>% fit(epochs = 120, early_stopping = F, 
+                             validation_split = 0.1)
+sin_lasso_test_torch %>% coef()
+
 lasso_test_tf %>% fit(epochs = 120, early_stopping = F, validation_split = 0)
 
 
@@ -93,20 +109,33 @@ lasso_test_torch %>% coef())
 
 
 lasso_test_torch <- deepregression(y = matrix(y), list_of_formulas = list(
-  loc = ~ -1 + lasso(x1,x2) + lasso(x3) + lasso(x4) + lasso(x5),
+  loc = ~ -1 + lasso(x1, la = 0.1) + lasso(x2, la = 0.1) + lasso(x3, la = 0.1) +
+    lasso(x4, la = 0.1) + lasso(x5, la = 0.1),
   scale = ~ 1), data = x, engine = "torch",
   subnetwork_builder = subnetwork_init_torch, model_builder = torch_dr,
   orthog_options = orthog_control(orthogonalize = F)
 )
 lasso_test_tf <- deepregression(y = matrix(y), list_of_formulas = list(
-  loc = ~ -1 + lasso(x1,x2) + lasso(x3) + lasso(x4) + lasso(x5),
+  loc = ~ -1 + lasso(x1, la = 1) + lasso(x2, la = 1) + lasso(x3, la = 1) +
+    lasso(x4, la = 1) + lasso(x5, la = 1),
   scale = ~ 1), data = x, engine = "tf",
   orthog_options = orthog_control(orthogonalize = F)
 )
-lasso_test
-lasso_test %>% fit(epochs = 150, early_stopping = T)
+
+sin_lasso_test_tf <- deepregression(y = matrix(y), list_of_formulas = list(
+  loc = ~ -1 + x1 + x2 + x3 + x4 + x5,
+  scale = ~ 1), data = x, engine = "tf",
+  orthog_options = orthog_control(orthogonalize = F)
+)
+
+lasso_test_tf %>% fit(epochs = 500, early_stopping = T, validation_split = 0.2)
+sin_lasso_test_tf %>% fit(epochs = 100, early_stopping = F, validation_split = 0.2)
 
 lasso_test_tf %>% coef()
+sin_lasso_test_tf %>% coef()
+
+
+lasso_test_torch %>% fit(epochs = 500, early_stopping = F, validation_split = 0.2)
 lasso_test_torch %>% coef()
 
 torch_multiply(
@@ -118,9 +147,26 @@ round(as.array(Reduce(f = "prod", lasso_test$model()$parameters[7:8])))
 
 
 
+debugonce(deepregression)
+rigde_test_tf <- deepregression(y = matrix(y), list_of_formulas = list(
+  loc = ~ -1 + ridge(x1, la = 0.1) + ridge(x2, la = 0.1) + ridge(x3, la = 0.1) +
+    ridge(x4, la = 0.1) + ridge(x5, la = 0.1),
+  scale = ~ 1), data = x, engine = "tf",
+  orthog_options = orthog_control(orthogonalize = F)
+)
+rigde_test_torch <- deepregression(y = matrix(y), list_of_formulas = list(
+  loc = ~ -1 + ridge(x1, la = 0.1) + ridge(x2, la = 0.1) + ridge(x3, la = 0.1) +
+    ridge(x4, la = 0.1) + ridge(x5, la = 0.1),
+  scale = ~ 1), data = x, engine = "torch",
+  subnetwork_builder = subnetwork_init_torch, model_builder = torch_dr,
+  orthog_options = orthog_control(orthogonalize = F)
+)
 
+rigde_test_tf %>% fit(epochs = 50, early_stopping = F, validation_split = 0.2)
+rigde_test_tf %>% coef()
 
-
+rigde_test_torch %>% fit(epochs = 50, early_stopping = F, validation_split = 0.2)
+rigde_test_torch %>% coef()
 
 
 
