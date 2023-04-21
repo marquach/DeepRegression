eeda95ab183a2a742a2ad1e2ae3d0e29b4188a23
Christopher Marquardt <ch.marquardt@campus.lmu.de>
2023-04-21 13:55:29 +0200

    caret:: added

--- tests/categorical_variables.R
+++ tests/categorical_variables.R
@@ -7,7 +7,7 @@ x <- sample(1:3, size = 1000, replace = T)
 x <- as.factor(x)
 x <- data.frame(x)
 
-x_help <- dummyVars(" ~ .", data = x)
+x_help <- caret::dummyVars(" ~ .", data = x)
 x_onehot <- data.frame(predict(x_help, newdata = x))
 beta <- c(2,5,2)
 
