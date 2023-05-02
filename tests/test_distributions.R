a18d006a473fd6282d047392c4e2ba7c23c3a055
Christopher Marquardt <ch.marquardt@campus.lmu.de>
2023-05-02 10:54:09 +0200

    more epochs and coefs comparison

--- tests/test_distributions.R
+++ tests/test_distributions.R
@@ -9,16 +9,16 @@ x <- seq(0, 1, 0.001)
 y <- rpois(n = length(x), lambda = exp(2*x))
 orthog_options = orthog_control(orthogonalize = F)
 
-plot(density(y))
-
 glm(formula = y~x, family = poisson)
 
 data_poisson <- data.frame(x)
 
+#not sure why it does not work
 model_poisson_tf <- deepregression(y = y,
                                    list_of_formulas = list( rate = ~1 + x),
                data = data_poisson,
                family = "poisson")
+
 model_poisson_tf %>% fit(epochs = 100, validation_split = 0.2)
 
 model_poisson_torch <- deepregression(y = y,
@@ -29,13 +29,14 @@ model_poisson_torch <- deepregression(y = y,
                                    orthog_options = orthog_options,
                                    subnetwork_builder = subnetwork_init_torch,
                                    model_builder = torch_dr)
-model_poisson_torch %>% fit(epochs = 100, validation_split = 0.2)
+model_poisson_torch %>% fit(epochs = 300, validation_split = 0.2)
 model_poisson_torch %>% coef()
 
 set.seed(42)
 n <- 10000
 x <- seq(0, 1, length.out = n)
 y <- rgamma(n, shape = 1, rate = 0.1)
+data_gamma <- data.frame(x)
 # beta = rate
 # alpha = shape (concentration)
 
@@ -44,25 +45,26 @@ glm(y~1,family=Gamma)
 model_gamma_tf <- deepregression(y = y,
                                     list_of_formulas = list( rate = ~1,
                                                              shape = ~1),
-                                    data = data_poisson,
+                                    data = data_gamma,
                                     family = "gamma",
                                     engine = "tf",
                                     orthog_options = orthog_options)
-model_gamma_tf %>% fit(epochs = 100, validation_split = 0.2)
+model_gamma_tf %>% fit(epochs = 300, validation_split = 0.2)
 model_gamma_tf %>% coef(which_par = 1)
 model_gamma_tf %>% coef(which_par = 2)
 
 model_gamma_torch <- deepregression(y = y,
                                       list_of_formulas = list( rate = ~1,
                                                                shape = ~1),
-                                      data = data_poisson,
+                                      data = data_gamma,
                                       family = "gamma",
                                       engine = "torch",
                                       orthog_options = orthog_options,
                                       subnetwork_builder = subnetwork_init_torch,
                                       model_builder = torch_dr)
-model_gamma_torch %>% fit(epochs = 100, validation_split = 0.2)
-model_gamma_torch %>% coef()
+model_gamma_torch %>% fit(epochs = 300, validation_split = 0.2)
+model_gamma_torch %>% coef(which_par = 1)
+model_gamma_torch %>% coef(which_par = 2)
 
 
 set.seed(42)
