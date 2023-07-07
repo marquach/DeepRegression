fa69690a988d16c99e17a442a5f897bf19093662
Christopher Marquardt <ch.marquardt@campus.lmu.de>
2023-07-07 14:39:09 +0200

    find better solution for datalaoder preparation

--- deepregression-main/R/data_handler_torch.R
+++ deepregression-main/R/data_handler_torch.R
@@ -16,11 +16,13 @@ prepare_data_torch <- function(pfc, input_x, target = NULL, object = NULL){
   df_list <- lapply(distr_datasets_index, function(x){ input_x[x] })
   
   if(!is.null(object)){
-    if(sum(names(object$model()$modules) == "subnetwork")==1){
+    if(sum(names(object$model()$modules) == "subnetwork") == 1){
     df_list <- df_list[1]}
   }
-  if(object$init_params$family == "bernoulli") df_list <- c(df_list, df_list)
   
+  if(attr(make_torch_dist(object$init_params$family), "nrparams_dist") == 1){
+    df_list <- c(df_list, df_list)
+    }
   if(is.null(target)) return(df_list)
   
   get_luz_dataset(df_list = df_list, target = torch_tensor(target)$to(torch_float()))
@@ -32,11 +34,33 @@ prepare_input_list_model <- function(input_x, input_y,
                                      callbacks = NULL, verbose, view_metrics, 
                                      early_stopping){
   
+  if(object$engine == 'tf'){
+    
+    input_list_model <-
+      list(object = object$model,
+           epochs = epochs,
+           batch_size = batch_size,
+           validation_split = validation_split,
+           validation_data = validation_data,
+           callbacks = callbacks,
+           verbose = verbose,
+           view_metrics = ifelse(view_metrics,
+                                 getOption("keras.view_metrics",
+                                           default = "auto"), FALSE)
+      )
+    
+    input_list_model <- c(input_list_model,
+                          list(x = input_x, y = input_y))
+    
+    return(input_list_model)
+  }
+  
   if(object$engine == "torch"){
     input_dataloader <- prepare_data_torch(
       pfc  = object$init_params$parsed_formulas_content,
       input_x = input_x,
-      target = input_y, object = object)
+      target = input_y,
+      object = object)
     
     #no validation
     if(is.null(validation_data) & identical(validation_split, 0)){
@@ -45,19 +69,22 @@ prepare_input_list_model <- function(input_x, input_y,
     }
     
     if(!is.null(validation_data)){
+      
       train_dl <- dataloader(input_dataloader, batch_size = batch_size,
                              shuffle = T)
       
       validation_dataloader <- prepare_data_torch(
         pfc  = object$init_params$parsed_formulas_content,
         input_x = validation_data[[1]],
-        target = validation_data[[2]])
+        target = validation_data[[2]],
+        object = object)
       
       valid_dl <- dataloader(validation_dataloader, batch_size = batch_size,
                              shuffle = T)
     }
     
     if(!identical(validation_split, 0) & !is.null(validation_split)){
+      
       train_ids <- 1:(ceiling((1-validation_split) * length(input_y)))
       valid_ids <- setdiff(1:length(input_y), train_ids)
 
@@ -67,6 +94,7 @@ prepare_input_list_model <- function(input_x, input_y,
       if(any(unlist(lapply(input_x, check_data_for_image)))) 
         cat(sprintf("Found %s validated image filenames \n", length(train_ids)))
       train_dl <- dataloader(train_ds, batch_size = batch_size, shuffle = T)
+      
       if(any(unlist(lapply(input_x, check_data_for_image))))
         cat(sprintf("Found %s validated image filenames \n", length(valid_ids)))
       valid_dl <- dataloader(valid_ds, batch_size = batch_size, shuffle = T)
@@ -82,26 +110,9 @@ prepare_input_list_model <- function(input_x, input_y,
       valid_data = valid_data,
       callbacks = callbacks,
       verbose = verbose)
-    return(c(input_list_model))
+    
+    c(input_list_model)
   }
   
-  if(object$engine == 'tf'){
-    input_list_model <-
-      list(object = object$model,
-           epochs = epochs,
-           batch_size = batch_size,
-           validation_split = validation_split,
-           validation_data = validation_data,
-           callbacks = callbacks,
-           verbose = verbose,
-           view_metrics = ifelse(view_metrics,
-                                 getOption("keras.view_metrics",
-                                           default = "auto"), FALSE)
-      )
-    
-    input_list_model <- c(input_list_model,
-                          list(x = input_x, y = input_y)
-    )
-    
-    input_list_model}
+  
 }
